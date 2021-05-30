076a09eebb2c11b080bcc0f485419b66cf9d65d6
hostsroth <85051095+hostsroth@users.noreply.github.com>
2021-05-30 18:00:46 +0800

    Update index.js

--- .netlify/functions/index.js
+++ .netlify/functions/index.js
@@ -8,5 +8,8 @@ client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
   console.log(err ? err.stack : res.rows[0].message) // Hello World!
   client.end()
 })
-
+return {
+  statusCode: 200,
+  body: err
+};
 }
