74c754a983488c11396b2cf725a419c48dbab043
hostsroth <85051095+hostsroth@users.noreply.github.com>
2021-05-30 17:50:18 +0800

    Update index.js

--- .netlify/functions/index.js
+++ .netlify/functions/index.js
@@ -2,10 +2,7 @@ exports.handler = async function(event, context) {
   const pg = require('pg');
   const conString = "postgres://pkqsbedc:CxPicJdBE6x4gh_rEyNurjkLf5awPMzF@batyr.db.elephantsql.com/pkqsbed";
   const client = new pg.Client(conString);
-  client.connect(function(err) {
-    return {
-      statusCode: 200,
-      body: err
-    }
-  });
+  client.connect();
+  const res = client.query('SELECT $1::text as message', ['Hello world!']);
+  console.log(res.rows[0].message);
 }
